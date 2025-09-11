a2c1b3f66c86b147219fca05fe508774b418109a
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-09-11 14:01:02 +0900

    Create LED.cpp

---
+++ LED.cpp
@@ -0,0 +1,21 @@
+* Created: 2025-09-11 오후 1:45:33
+ * Author : GC
+ */ 
+// 14.7mhz 로 동작
+#define F_CPU 14745600UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+	DDRB = 0xff;
+    while (1) 
+    {
+		PORTB = 0xff;
+		_delay_ms(500);
+		PORTB = 0x00;
+		_delay_ms(500);
+    }
+	return 0;
+}
